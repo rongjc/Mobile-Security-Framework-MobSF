e2ce9c99c3e7cf6e9668fcbb581d8b09187e199a
JC <jrong@netflix.com>
2024-07-30 12:30:17 +0800

    test

--- mobsf/script/check_similarity.sh
+++ mobsf/script/check_similarity.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Set this to 1 to enable echo statements, or 0 to disable them
-ECHO=1
-
-function log {
-    if [ "$ECHO" -eq 1 ]; then
-        echo "$1"
-    fi
-}
-
-log "Started the script"
+echo "Started the script"
 
 # Retrieve the parameters
 first_hash=$1
@@ -19,24 +10,24 @@ second_file=$4
 output_file=$5
 filte_temp_path=$6
 
-log "Parameters received:"
-log "First hash: $first_hash"
-log "Second hash: $second_hash"
-log "First file: $first_file"
-log "Second file: $second_file"
-log "Output file: $output_file"
-log "Filter temporary path: $filte_temp_path"
+echo "Parameters received:"
+echo "First hash: $first_hash"
+echo "Second hash: $second_hash"
+echo "First file: $first_file"
+echo "Second file: $second_file"
+echo "Output file: $output_file"
+echo "Filter temporary path: $filte_temp_path"
 
 # Define the prefix directory
 prefix_directory="/path/to/your/prefix"
 
 # Get the directory of the current script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-log "Script directory: $SCRIPT_DIR"
+echo "Script directory: $SCRIPT_DIR"
 
 # Construct the path to the Python script
 PYTHON_SCRIPT="$SCRIPT_DIR/process_html.py"
-log "Python script path: $PYTHON_SCRIPT"
+echo "Python script path: $PYTHON_SCRIPT"
 
 # Array of directories to remove files from
 directories=(
@@ -74,60 +65,61 @@ directories=(
     "com/appsflyer/"
 )
 
-log "First parameter: $first_file"
-log "Second parameter: $second_file"
-log "Output file: $output_file"
-log "Writing 'Started the script' to $filte_temp_path"
+# Example operation with the parameters
+echo "First parameter: $first_file"
+echo "Second parameter: $second_file"
+echo "Output file: $output_file"
+echo "Writing 'Started the script' to $filte_temp_path"
 echo "Started the script" > $filte_temp_path
 
 # Add your script logic here
-log "Removing old output file if it exists: $output_file"
+echo "Removing old output file if it exists: $output_file"
 rm -f $output_file
 
-log "Changing directory to ./test"
+echo "Changing directory to ./test"
 cd ./test
 
-log "Removing old directory: ../$first_hash-$second_hash"
+echo "Removing old directory: ../$first_hash-$second_hash"
 rm -rf ../$first_hash-$second_hash
 
-log "Creating new directory: $first_hash-$second_hash"
+echo "Creating new directory: $first_hash-$second_hash"
 mkdir $first_hash-$second_hash
 
-log "Changing directory to: $first_hash-$second_hash"
+echo "Changing directory to: $first_hash-$second_hash"
 cd $first_hash-$second_hash
 
-log "Copying $first_file to ./$first_hash"
+echo "Copying $first_file to ./$first_hash"
 cp -rf $first_file ./$first_hash
 
-log "Copying $second_file to ./$second_hash"
+echo "Copying $second_file to ./$second_hash"
 cp -rf $second_file ./$second_hash
 
 # Remove all files from each directory
 for dir in "${directories[@]}"; do
     full_path="./$first_hash/$dir"
-    log "Removing files from $full_path"
+    echo "Removing files from $full_path"
     rm -rf "$full_path"*
 
     full_path="./$second_hash/$dir"
-    log "Removing files from $full_path"
+    echo "Removing files from $full_path"
     rm -rf "$full_path"*
 done
 
-log "All specified files have been removed."
+echo "All specified files have been removed."
 
-log "Running nicad6cross"
+echo "Running nicad6cross"
 nicad6cross functions java ./$first_hash ./$second_hash type3-2-report
 
-log "Copying HTML report to $output_file"
+echo "Copying HTML report to $output_file"
 cp "./${first_hash}_functions-blind-crossclones/${first_hash}_functions-blind-crossclones-0.30-classes-withsource.html" $output_file
 
-log "Running Python script on $output_file"
+echo "Running Python script on $output_file"
 python3 "$PYTHON_SCRIPT" "$output_file"
 
-log "Removing directory: ../$first_hash-$second_hash"
+echo "Removing directory: ../$first_hash-$second_hash"
 rm -rf ../$first_hash-$second_hash
 
-log "Removing filter temporary file: $filte_temp_path"
+echo "Removing filter temporary file: $filte_temp_path"
 rm $filte_temp_path
 
-log "Script finished"
+echo "Script finished"
